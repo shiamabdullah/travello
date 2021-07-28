8d87a037db6c53e6676e36728f7ceb76169aaf30
shiam <shiamabdullah1@gmail.com>
2021-07-28 20:43:44 +0600

    reg done with automigrate

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,20 @@
 from django.http import response
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User,auth
 
 # Create your views here.
 def register(request):
-    return render(request,'register.html')
+    if (request.method=='POST'):
+        first_name= request.POST['first_name']
+        last_name= request.POST['last_name']
+        username= request.POST['username']
+        password= request.POST['last_name']
+        password2= request.POST['password2']
+        email= request.POST['email']
+
+        user= User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+        user.save();
+        print('user created')
+        return redirect('/')
+    else:
+        return render(request,'register.html')
