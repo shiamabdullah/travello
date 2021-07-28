7525ef10b9fe32ea9385553c6e1abc937701b076
shiam <shiamabdullah1@gmail.com>
2021-07-28 19:51:57 +0600

    added cities

--- travello/models.py
+++ travello/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class Destination(models.Model):
     name = models.CharField(max_length=100) 
     img = models.ImageField(upload_to='pics')
-    desc = models.TextField
-    price = models.IntegerField
+    desc = models.TextField()
+    price = models.IntegerField()
     offer = models.BooleanField(default=False)
