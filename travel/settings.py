3a73dc9e3e3f7e724396bf65bebf19471a67eefb
shiam <shiamabdullah1@gmail.com>
2021-10-09 02:48:11 +0600

    ss

--- travel/settings.py
+++ travel/settings.py
@@ -134,6 +134,10 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 MEDIA_URL='/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
