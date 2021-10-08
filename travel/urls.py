6176eb2448b24909955424b9bcf78134f4000af0
shiam <shiamabdullah1@gmail.com>
2021-10-08 19:01:14 +0600

    aaa

--- travel/urls.py
+++ travel/urls.py
@@ -26,8 +26,7 @@ urlpatterns = [
     path('', include('travello.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+  
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
