5b71035e1af29e2064cacf11c90c5c1ec538bcd1
shiam <shiamabdullah1@gmail.com>
2021-10-08 19:19:39 +0600

    sss

--- travel/settings.py
+++ travel/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,7 +133,6 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 MEDIA_URL='/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
