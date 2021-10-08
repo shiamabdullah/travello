59f4d7d5925ecbdbbf3975ceef093805848053e4
shiam <shiamabdullah1@gmail.com>
2021-10-08 14:36:45 +0600

    adding ignore

--- accounts/views.py
+++ accounts/views.py
@@ -11,10 +11,17 @@ def register(request):
         password= request.POST['last_name']
         password2= request.POST['password2']
         email= request.POST['email']
-
-        user= User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
-        user.save();
-        print('user created')
+        if (password==password2):
+            if(User.objects.filter(username=username).exists()):
+                user= User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                user.save();
+                print('user created')
+            else:
+                print('username already exists')
+        else:
+            print('password not matching')
+        
         return redirect('/')
+
     else:
         return render(request,'register.html')
